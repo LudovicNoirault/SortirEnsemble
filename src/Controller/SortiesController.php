a50d9aadf5612b9bc8d25ea39ef88bba14e7b091
AlexisPontoizeau <alexis.pontoizeau@orange.fr>
2019-09-18 11:11:56 +0200

    Modif Form Sorties

--- src/Controller/SortiesController.php
+++ src/Controller/SortiesController.php
@@ -5,6 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Entity\Sorties;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\SortiesForm;
+
+
+
 class SortiesController extends AbstractController
 {
     /** 
@@ -16,21 +25,21 @@ class SortiesController extends AbstractController
     }
 
     /** 
+    * @Route("/sorties/create", name="sortie_create")
+    */
+    public function createSortie()
+    {
+        $request = new Request();
+        return $this->new($request);
+    }
+
+    /** 
     * @Route("/sorties/{id}", name="sortie")
     */
     public function singleSortie($id)
     {
         return $this->render('sorties/singleSortie.html.twig');
     }
-
-    /** 
-    * @Route("/sorties/creation", name="sortie_create")
-    */
-    public function createSortie()
-    {
-        return $this->render('sorties/createSortie.html.twig');
-    }
-
     /** 
     * @Route("/sorties/{id}/update", name="sortie_update")
     */
@@ -61,5 +70,31 @@ class SortiesController extends AbstractController
     public function sites()
     {
         return $this->render('sites.html.twig');
+    }
+
+    public function new(Request $request)
+    {
+        // creates a sorties object and initializes some data for this example
+        $sorties = new Sorties();
+        $form = $this->createForm(SortiesForm::class, $sorties);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            $sorties = $form->getData();
+
+            // ... perform some action, such as saving the task to the database
+            // for example, if Task is a Doctrine entity, save it!
+            // $entityManager = $this->getDoctrine()->getManager();
+            // $entityManager->persist($task);
+            // $entityManager->flush();
+
+        return $this->redirectToRoute('task_success');
+        }
+
+        return $this->render('create_sorties.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
