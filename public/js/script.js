743c347000881945e7e6027b95a4d50b2d18435f
pfavreau <philippe.favreau@imie.fr>
2019-09-19 12:23:15 +0200

    more triangles

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,15 @@
 /* lol je skrypte */
+function burgerActivate(){
+	let navbox = document.getElementById('header-nav-box');
+	let dasBurger = document.getElementById('header-burger');
+	if (navbox.style.right == '-20rem') {
+		navbox.style.right = '0rem';
+		dasBurger.style.top = '10%';
+	}
+	else {
+		navbox.style.right = '-20rem';
+		dasBurger.style.top = '0%';
+	}
+}
+let burger = document.getElementById('header-burger');
+burger.addEventListener("click", burgerActivate);
